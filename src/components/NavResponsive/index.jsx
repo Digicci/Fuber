3bee9ac7133d784d75f02694beca8ff3ba1322c1
Orima574 <boblion574@outlook.fr>
2022-12-20 19:33:05 +0100

    add nav responsive

--- src/components/NavResponsive/index.jsx
+++ src/components/NavResponsive/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import colors from "../../colors";
-// import logo from '../../assets/logo.webp';
-// import { StyledLink } from "../../utils/Atoms";
+import logo from '../../assets/logo.webp';
+import { StyledLink } from "../../utils/Atoms";
+import { useAuth } from "../../utils/hook/useAuth";
 
 
 const StyledNavResponsive = styled.div`
@@ -35,14 +36,42 @@ const StyledClose = styled.div`
 const StyledContainer = styled.div`
     width: 33%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
     background : ${colors.primary};
+    justify-content: flex-start;
     @media (max-width: 425px){
         width:80%;
     }
+    @media (min-width: 1440px){
+        width: 22%;
+    }
+`
+
+const StyledConnectionWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    border-bottom: solid ${colors.secondary} 1px;
+    padding-bottom: 2rem;
+    margin-bottom: 2rem;
+`
+const LogoNav = styled.div`
+    width: 100%;
+    overflow: hidden;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    margin-bottom: 2rem;
+`
+const ImgNav = styled.img`
+    width: 150px;
 `
 
 function NavResponsive({isOpen, toggle}){
     
+    const {user, signout, isConnected} = useAuth()
 
     return (
         <>
@@ -51,6 +80,58 @@ function NavResponsive({isOpen, toggle}){
                     <StyledClose>
                         <i class="ph-x closemenu"></i>
                     </StyledClose>
+                    {isConnected() ? (
+                            <>
+                                <StyledConnectionWrap>
+                                    <StyledLink to="/user" $disappearance>
+                                        {user?.nom}, {user?.prenom}
+                                    </StyledLink>
+                                    <StyledLink to="/login" $isFullLink $disappearance $xxl onClick={signout}>
+                                        Déconnexion
+                                    </StyledLink>
+                                    <StyledLink $navVertical>
+                                        Mes course
+                                    </StyledLink>
+                                    <StyledLink $navVertical>
+                                        Wallet
+                                    </StyledLink>
+                                    <StyledLink $navVertical>
+                                        Parametres du profil
+                                    </StyledLink>
+                                </StyledConnectionWrap>
+                                <StyledLink to='' $navVertical>
+                                    Commandez une course
+                                </StyledLink>
+                                <StyledLink $navVertical>
+                                    Devenir partenaire
+                                </StyledLink>
+                            </>
+                        ) : (
+                            <>
+                               <StyledConnectionWrap>
+                                <StyledLink to="/signup" $isShadowLink $disappearance $xxl>
+                                    Inscription
+                                    </StyledLink>
+                                    <StyledLink to="/login" $isFullLink $disappearance $xxl>
+                                    Connexion
+                                </StyledLink>
+                               </StyledConnectionWrap>
+                               <StyledLink to='/login' $navVertical>
+                                    Commandez une course
+                                </StyledLink>
+                                <StyledLink $navVertical>
+                                    Devenir partenaire
+                                </StyledLink>
+                                <StyledLink $navVertical>
+                                    Aide
+                                </StyledLink>
+                            </>
+                    )}
+                    <LogoNav>
+                        <StyledLink to='/'>
+                            <ImgNav src={logo} alt="logo" />
+                        </StyledLink>
+                    </LogoNav>
                 </StyledContainer>
             </StyledNavResponsive>
         </>
