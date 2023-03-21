5a20cbd4f40ee3d408f213dfe6ca9ff2e991bde1
Brasero <brandonricci4@gmail.com>
2023-03-22 00:23:32 +0100

    fetching driver

--- src/components/Client/CarCards/index.jsx
+++ src/components/Client/CarCards/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import CarCard from "../CarCard";
 import { useRace } from "../../../utils/hook/Client/useRace";
@@ -6,13 +6,27 @@ import Driver from "../../../utils/Data/Client/Driver";
 import {
     TypeChoiceCar,
 } from "./atoms"
-
-
-
+import { useAxios } from "../../../utils/hook/useAxios";
+import { useLocation } from "../../../utils/hook/useLocation";
+import { useCsrf } from "../../../utils/hook/useCsrf";
 
 function CarCards(){
 
     const race = useRace()
+    const axios = useAxios()
+    const location = useLocation()
+    const csrf = useCsrf()
+
+    useEffect(() => {
+        axios.post('user/getNearDrivers', {
+            lat: location.location.lat,
+            lng: location.location.lng,
+            _csrf: csrf.token
+        }).then((res) => {
+            console.log(res.data)
+            setData(res.data)
+        })
+    }, [])
 
     const handleCarChoice = (id, total, driverPrice, enterprise) => {
         race.setRaceInfo({
@@ -25,13 +39,13 @@ function CarCards(){
         console.log({total, driverPrice, enterprise, id, race: race.raceInfo})
     }
 
-    const [data,setData] = useState(Driver)
+    const [data,setData] = useState()
 
     return(
         <>
             <TypeChoiceCar>
                 {
-                    data.map((car) => {
+                    data?.map((car) => {
                         return <CarCard {...car} handleClick={handleCarChoice}/>
                     })
                 }
