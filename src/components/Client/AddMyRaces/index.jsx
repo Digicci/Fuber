a828d5f811e25ac6928a95271eed3adffa6ea6d1
Brasero <brandonricci4@gmail.com>
2023-03-23 00:34:49 +0100

    fetching pending races

--- src/components/Client/AddMyRaces/index.jsx
+++ src/components/Client/AddMyRaces/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
     MyRaceH3,
     ContainerMyRaces,
@@ -10,37 +10,72 @@ import {
     ButtonRaceFinish
 } from "../../../utils/Atoms";
 import Hybride from "../../../assets/hybride.webp"
+import confort from "../../../assets/confort.webp"
+import van from "../../../assets/van.webp"
+import {useAxios} from "../../../utils/hook/useAxios";
 
 
-function AddMyRace(){
+function AddMyRace() {
 
-    return(
+    const axios = useAxios()
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get('race/getAllPending').then((res) => {
+            console.log(res.data)
+            setData(res.data)
+        })
+    }, [])
+
+    return (
         <>
             <ContainerMyRaces>
-                <MyRaceH3>
-                    Il semble que vous n'avez pas de course en cours.
-                </MyRaceH3>
-                <RaceInProgress>
-                    Courses en cours
-                </RaceInProgress>
-                <DivRace>
-                    <RaceImg src={Hybride} alt="hybrid car"/>
-                    <InfoRace>
-                        <h4>Comfort Hybride par Prenom N.</h4>
-                        <h5>Numero du chauffeur</h5>
-                        <h5>13.40€</h5>
-                        <h5>Adresse départ</h5>
-                        <h5>Adresse arrivé</h5>
-                        <h5>Date / heure</h5>
-                    </InfoRace>
-                    <DivButton>
-                        <ButtonRaceFinish>
-                            Course terminer
-                        </ButtonRaceFinish>
-                    </DivButton>
-                </DivRace>
+                {data.length < 0 ? (
+                    <MyRaceH3>
+                        Il semble que vous n'avez pas de course en cours.
+                    </MyRaceH3>
+                ) : (
+                    <>
+                        <RaceInProgress>
+                            Courses en cours
+                        </RaceInProgress>
+                        {
+                            data.map((race) => {
+                                const dateObj = new Date(race.createdAt)
+                                const date = dateObj.getDate() + "/" + (dateObj.getMonth() + 1) + "/" + dateObj.getFullYear()
+                                const time = dateObj.getHours() + ":" + dateObj.getMinutes()
+                                const entreprise = race.entreprise
+                                const car = entreprise.vehicule
+                                const img = car.img_path
+                                const src = img === "confort" ? confort : img === "hybride" ? Hybride : van
+                                return (
+                                    <DivRace>
+                                        <RaceImg src={src} alt="car img"/>
+                                        <InfoRace>
+                                            <h4>{car.type.toUpperCase()} par {entreprise.prenom}</h4>
+                                            <h5>{entreprise.num}</h5>
+                                            <h5>{(race.total / 100).toFixed(2)}€</h5>
+                                            <h5>Départ : {race.start}</h5>
+                                            <h5>Arrivée : {race.end}</h5>
+                                            <h5>{`${date} à ${time}`}</h5>
+                                        </InfoRace>
+                                        <DivButton>
+                                            <ButtonRaceFinish>
+                                                Course terminer
+                                            </ButtonRaceFinish>
+                                        </DivButton>
+                                    </DivRace>
+                                )
+                            })
+                        }
+                    </>
+                )
+                }
+
+
             </ContainerMyRaces>
         </>
     )
 }
+
 export default AddMyRace
