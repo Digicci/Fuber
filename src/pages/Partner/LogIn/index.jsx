ce9e72ed3be17ecfb70c232e4783b1cae3977206
Brasero <brandonricci4@gmail.com>
2023-07-27 15:34:31 +0200

    connected login page to authStore resolve connexion bug

--- src/pages/Partner/LogIn/index.jsx
+++ src/pages/Partner/LogIn/index.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import {useSelector} from "react-redux";
+import { getAuth } from "../../../utils/store/Partner/selectors/AuthSelectors";
 import {
     Close,
     Container,
@@ -10,21 +12,22 @@ import {
 import darkLogo from "../../../assets/driver/logodriverreverse.webp"
 import logo from "../../../assets/driver/logodriver.webp"
 import FormLogin from "../../../components/Partner/FormLogin";
-import { useAuthEntreprise } from "../../../utils/hook/Partner/useAuthEntreprise";
 
 
 function LogIn(){
 
-    const auth = useAuthEntreprise()
+    const auth = useSelector(getAuth)
     const navigate = useNavigate()
     const location = useLocation()
     const {from} = location.state || {from: {pathname: "/partner/account/home"}}
 
     useEffect(() => {
-        if(auth.isConnected()){
+        if(auth.auth){
             navigate(from, {replace: true})
+            console.log("connected")
         }
-    }, [auth.entreprise])
+        console.log(auth.auth)
+    }, [auth.auth])
     return (
         <>
             <Container>
